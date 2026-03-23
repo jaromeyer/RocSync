cd8e34f37737192bf127a044ec55dec15c0ca4e2
Jaro Meyer <jaro.meyer@protonmail.com>
2026-03-23 17:16:00 +0100

    [hw] add note about HSE bypass

--- fw/src/funconfig.h
+++ fw/src/funconfig.h
@@ -3,10 +3,10 @@
 
 // Place configuration items here, you can see a full list in ch32fun/ch32fun.h
 // To reconfigure to a different processor, update TARGET_MCU in the  Makefile
-#define FUNCONF_USE_HSE 1    // external crystal on PA1 PA2
-#define FUNCONF_USE_HSI 0    // internal 24MHz clock oscillator
-#define FUNCONF_USE_PLL 0    // use PLL x2
-#define FUNCONF_HSE_BYPASS 0 // bypass the HSE when using an external clock source
+#define FUNCONF_USE_HSE 1    // enable external 24MHz oscillator on PA1 PA2
+#define FUNCONF_USE_HSI 0    // disable internal 24MHz oscillator
+#define FUNCONF_USE_PLL 0    // disable PLL x2 for 24MHz system clock
+#define FUNCONF_HSE_BYPASS 0 // use HSE circuit (amplifier?) despite external oscillator (not crystal) since it outputs clipped sine-wave instead of TTL levels
 
 #define FUNCONF_SYSTICK_USE_HCLK 1
 
